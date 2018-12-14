7ab362ecdc01402e7b6ae2e4760797c45b3868f8
Georgi Yankov <spookyfox@abv.bg>
2018-12-14 11:40:10 +0200

    Archive view now available.

---
+++ src/LearningPlus.Web.ViewModels/News/NewsArchiveViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace LearningPlus.Web.ViewModels.News
+{
+    public class NewsArchiveViewModel
+    {
+        [DisplayName("#")]
+        public string Id { get; set; }
+
+        [DisplayName("Създадена на")]
+        public DateTime CreatedOn { get; set; }
+
+        [DisplayName("Създател")]
+        public string Creator { get; set; }
+
+        [DisplayName("Текст")]
+        public string Message { get; set; }
+
+        [DisplayName("Целеви групи")]
+        public string TargetRoles { get; set; }
+
+        [DisplayName("Изтекла на")]
+        public DateTime ExpiresOn { get; set; }
+    }
+}
