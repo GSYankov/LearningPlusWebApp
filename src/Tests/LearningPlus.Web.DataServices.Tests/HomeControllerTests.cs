d80c6b2f99ed17a5694fb056b7dc3d6e4b0748fd
Georgi Yankov <spookyfox@abv.bg>
2019-01-03 15:47:09 +0200

    Google iframe Selenium test implemented

---
+++ src/Tests/LearningPlus.Web.DataServices.Tests/HomeControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Shouldly;
+using Xunit;
+using System;
+
+namespace LearningPlus.Web.DataServices.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexView_GoogleMapsIframeExists()
+        {
+            var driver = new ChromeDriver(".");
+            driver.Navigate().GoToUrl("https://localhost:44310");
+            driver.Manage().Window.Minimize();
+            driver.SwitchTo().Frame(driver.FindElement(By.TagName("iframe")));
+            IWebElement iframe = null;
+            try
+            {
+                iframe = driver.FindElement(By.TagName("pre"));
+            }
+            catch (Exception)
+            {
+
+            }
+
+            iframe.ShouldBeNull();
+        }
+    }
+}
